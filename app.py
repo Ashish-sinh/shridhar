275ed26f492de32b6bfd8230c5b2e72aebb5b74a
ashish12iya <ashishbaraiya0416@gmail.com>
2025-09-18 22:02:15 +0530

    updated endpoint

--- app.py
+++ app.py
@@ -421,7 +421,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",  # This should match your filename
         host="0.0.0.0",
-        port=8000,
+        port=8001,
         reload=True,  # Set to False in production
         workers=2,  # Adjust based on your server capacity
         log_level="info",
